4409a5bcce53207fc77a2dcef7bcaa690861fa8a
btbeabout <bryan.beabout@gmail.com>
2024-08-21 14:06:41 -0400

    Completed Player section.

--- main.py
+++ main.py
@@ -3,19 +3,49 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    
+    ### Creating Screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    ### Setting FPS
+    clock = pygame.time.Clock()
+    # delta time
+    dt = 0
+
+    # Creating PyGame groups for easy updating
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Adding Player to Groups
+    Player.containers = (updatable, drawable)
+
+    ### Creating Player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    ### Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0,0,0))
-        pygame.display.flip()
+        
+        # Update Units:
+        for unit in updatable:
+            unit.update(dt)
+    
+        # Draw screen
+        screen.fill("black")
+        
+        # Draw Units
+        for unit in drawable:
+            unit.draw(screen)
 
+        pygame.display.flip()        
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
